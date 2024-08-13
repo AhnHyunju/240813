abf4c17746f72ca58d80d2a05d3346441519d49e
AhnHyunju <104109203+AhnHyunju@users.noreply.github.com>
2024-08-13 15:56:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import settings
 #load_dotenv()
 
 # python -m streamlit run main.py
-st.title("📝피터팬팬 전용 GPT(테디노트 이경록 대표님 코드 기반)")
+st.title("📝피터팬 전용 GPT(테디노트 이경록 대표님 코드 기반)")
 
 config = settings.load_config()
 if "api_key" in config:
